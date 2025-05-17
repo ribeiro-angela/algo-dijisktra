4b6927c143aa104cb4d56a46a96ffbfef5900cfb
Anastasia <anastasia.minkov@etu.u-paris.fr>
2025-05-17 21:13:32 +0200

    Add files via upload

--- SAE-LIBERABLE-VF/src/adaptator/GraphMaze.java
+++ SAE-LIBERABLE-VF/src/adaptator/GraphMaze.java
@@ -4,27 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 import graph.Graph;
-import graph.Graph.Arc;
 import maze.Maze;
 
 /**
  * Adaptateur entre Maze et Graph.
  * Transforme un labyrinthe en graphe non orienté non valué.
  */
-public class GraphMaze<T extends Integer> implements Graph<T> {
-    private final Maze maze;
+public class GraphMaze<T> implements Graph<T> {
+    private final Maze<T> maze;
 
-    public GraphMaze(Maze maze) {
+    public GraphMaze(Maze<T> maze) {
         this.maze = maze;
     }
 
-    @SuppressWarnings("unchecked")
     @Override
-    public List<Arc<T>> getSucc(T s) {
-        List<Arc<T>> arcs = new ArrayList<>();
-        for (Object neighbor : maze.openedNeighbours(s)) {
-            // Conversion explicite de Object vers T (qui est un Integer)
-            arcs.add(new Arc<>(1, (T) neighbor));
+    public List<Graph.Arc<T>> getSucc(T s) {
+        List<Graph.Arc<T>> arcs = new ArrayList<>();
+        for (T neighbor : maze.openedNeighbours(s)) {
+            arcs.add(new Graph.Arc<>(1, neighbor));
         }
         return arcs;
     }
